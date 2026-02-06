a7a289818b3b59025bab31aa80df60c81f345f0a
Vivek Karna <98736880+Vivekkarna4@users.noreply.github.com>
2026-02-06 15:08:46 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false, // 🚀 Disable Strict Mode
+};
+
+export default nextConfig;
---
+++ tailwind.config.mjs
@@ -0,0 +1,133 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+    	extend: {
+    		colors: {
+    			background: 'hsl(var(--background))',
+    			foreground: 'hsl(var(--foreground))',
+    			card: {
+    				DEFAULT: 'hsl(var(--card))',
+    				foreground: 'hsl(var(--card-foreground))'
+    			},
+    			popover: {
+    				DEFAULT: 'hsl(var(--popover))',
+    				foreground: 'hsl(var(--popover-foreground))'
+    			},
+    			primary: {
+    				DEFAULT: 'hsl(var(--primary))',
+    				foreground: 'hsl(var(--primary-foreground))'
+    			},
+    			secondary: {
+    				DEFAULT: 'hsl(var(--secondary))',
+    				foreground: 'hsl(var(--secondary-foreground))'
+    			},
+    			muted: {
+    				DEFAULT: 'hsl(var(--muted))',
+    				foreground: 'hsl(var(--muted-foreground))'
+    			},
+    			accent: {
+    				DEFAULT: 'hsl(var(--accent))',
+    				foreground: 'hsl(var(--accent-foreground))'
+    			},
+    			destructive: {
+    				DEFAULT: 'hsl(var(--destructive))',
+    				foreground: 'hsl(var(--destructive-foreground))'
+    			},
+    			border: 'hsl(var(--border))',
+    			input: 'hsl(var(--input))',
+    			ring: 'hsl(var(--ring))',
+    			chart: {
+    				'1': 'hsl(var(--chart-1))',
+    				'2': 'hsl(var(--chart-2))',
+    				'3': 'hsl(var(--chart-3))',
+    				'4': 'hsl(var(--chart-4))',
+    				'5': 'hsl(var(--chart-5))'
+    			}
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+    		animation: {
+    			'shiny-text': 'shiny-text 8s infinite',
+    			aurora: 'aurora 8s ease-in-out infinite alternate'
+    		},
+    		keyframes: {
+    			'shiny-text': {
+    				'0%, 90%, 100%': {
+    					'background-position': 'calc(-100% - var(--shiny-width)) 0'
+    				},
+    				'30%, 60%': {
+    					'background-position': 'calc(100% + var(--shiny-width)) 0'
+    				}
+    			},
+    			aurora: {
+    				'0%': {
+    					backgroundPosition: '0% 50%',
+    					transform: 'rotate(-5deg) scale(0.9)'
+    				},
+    				'25%': {
+    					backgroundPosition: '50% 100%',
+    					transform: 'rotate(5deg) scale(1.1)'
+    				},
+    				'50%': {
+    					backgroundPosition: '100% 50%',
+    					transform: 'rotate(-3deg) scale(0.95)'
+    				},
+    				'75%': {
+    					backgroundPosition: '50% 0%',
+    					transform: 'rotate(3deg) scale(1.05)'
+    				},
+    				'100%': {
+    					backgroundPosition: '0% 50%',
+    					transform: 'rotate(-5deg) scale(0.9)'
+    				}
+    			}
+    		}
+    	}
+    },
+	daisyui: {
+		themes: [
+			"light",
+			"dark",
+			"cupcake",
+			"bumblebee",
+			"emerald",
+			"corporate",
+			"synthwave",
+			"retro",
+			"cyberpunk",
+			"valentine",
+			"halloween",
+			"garden",
+			"forest",
+			"aqua",
+			"lofi",
+			"pastel",
+			"fantasy",
+			"wireframe",
+			"black",
+			"luxury",
+			"dracula",
+			"cmyk",
+			"autumn",
+			"business",
+			"acid",
+			"lemonade",
+			"night",
+			"coffee",
+			"winter",
+			"dim",
+			"nord",
+			"sunset",
+		],
+	},
+	plugins: [require("tailwindcss-animate"), require('daisyui')],
+};
